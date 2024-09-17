829a7ace0d097ae23afef806bdfa935c76a43da8
Jacobhjoorth <113351275+Jacobhjoorth@users.noreply.github.com>
2024-09-17 10:00:54 +0200

    PuzzleData(struct) and PuzzleManager(Actor) added

--- Source/PROD_GRUPP2/PROD_GRUPP2.Build.cs
+++ Source/PROD_GRUPP2/PROD_GRUPP2.Build.cs
@@ -11,6 +11,8 @@ public class PROD_GRUPP2 : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
---
+++ Source/PROD_GRUPP2/PuzzleData.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PuzzleData.generated.h"    // Include generated header
+
+
+// Struct
+USTRUCT(BlueprintType)
+struct PROD_GRUPP2_API FPuzzleData
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Puzzle")
+	FName PuzzleName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Puzzle")
+	bool bIsCompleted;
+
+	//Constructor to initialize default values
+	FPuzzleData()
+	{
+		PuzzleName = "DefaultPuzzle";
+		bIsCompleted = false;
+	}
+};
+
+
